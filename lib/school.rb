f2c096fe4d2dd42c98560639a49f65e0440f773b
Will Carter <will.carter@fergusllc.com>
2020-01-11 02:34:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,6 @@
 # code here!
 class School
-  attr_reader :name, :roster
+  attr_accessor :name, :roster
 
   def initialize(name)
     @name = name
@@ -8,19 +8,21 @@ class School
   end
 
   def add_student(student, grade)
-    if roster[grade] == nil
-      roster[grade] = []
+    p roster.object_id
+    p self.roster.object_id
+    if self.roster[grade] == nil
+      self.roster[grade] = []
     end
-    roster[grade] << student
+    self.roster[grade] << student
   end
 
   def grade(grade)
-    roster[grade]
+    self.roster[grade]
   end
 
   def sort
     sorted = {}
-    roster.each do |grade, students|
+    self.roster.each do |grade, students|
       sorted[grade] = students.sort
     end
     sorted
@@ -35,9 +37,9 @@ school = School.new("Test School")
 # p school.grade(10)
 
 school.add_student("Homer Simpson", 9)
-school.add_student("Bart Simpson", 9)
-school.add_student("Avi Flombaum", 10)
-school.add_student("Jeff Baird", 10)
-school.add_student("Blake Johnson", 7)
-school.add_student("Jack Bauer", 7)
-p school.sort
+# school.add_student("Bart Simpson", 9)
+# school.add_student("Avi Flombaum", 10)
+# school.add_student("Jeff Baird", 10)
+# school.add_student("Blake Johnson", 7)
+# school.add_student("Jack Bauer", 7)
+# p school.sort
